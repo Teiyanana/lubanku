71a16eb90411cbd690b1ebe1a763d3c2778c4bf8
tang <tang@TangdeMacBook-Pro.local>
2021-08-25 07:48:36 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Hello</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com">Baidu Link</Button>
+    </div>
+  );
+}
+
+export default App;
